ea40a69068511866849981e907be212d405b85e8
fgdamasceno <fgdamasceno@gmail.com>
2024-10-08 19:51:26 -0300

    primeiro programa java

--- java/capitulo01/src/Testes/Main.java
+++ java/capitulo01/src/Testes/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		final double CAMBIO_ATUAL = 5.54;
-		
+
 		System.out.println("Qual o valor em dolar?");
-		
+
 		double valorEmDolar = sc.nextDouble();
 		double valorEmReal = valorEmDolar * CAMBIO_ATUAL;
-		
+
 		System.out.printf("Valor em real: R$ %.2f", valorEmReal);
 
 		sc.close();
